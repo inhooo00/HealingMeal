c1c78de71f7d5c61c0ee7d1150ce7477d736aafa
jin <ekwls20@gmail.com>
2024-01-17 02:01:29 +0900

    필요한 findby 메소드 정의

--- healingmeal/src/main/java/gsc/healingmeal/data/repository/MainDishCategoryRepository.java
+++ healingmeal/src/main/java/gsc/healingmeal/data/repository/MainDishCategoryRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MainDishCategoryRepository extends JpaRepository<MainDishCategory, Long> {
-
+    MainDishCategory findById(long user_id);
 }

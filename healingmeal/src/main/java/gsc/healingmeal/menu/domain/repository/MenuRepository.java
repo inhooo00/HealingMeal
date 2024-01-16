539a041628ad5bbeb2f91f1fa3fef41f7eaf1637
jin <ekwls20@gmail.com>
2024-01-17 02:03:43 +0900

    식단 조합 repository

---
+++ healingmeal/src/main/java/gsc/healingmeal/menu/domain/repository/MenuRepository.java
@@ -0,0 +1,9 @@
+package gsc.healingmeal.menu.domain.repository;
+
+import gsc.healingmeal.menu.domain.MenuForUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuRepository extends JpaRepository<MenuForUser, Long> {
+}

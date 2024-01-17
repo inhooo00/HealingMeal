d312ba677cddfec91873db468bdaa1de3118c571
jin <ekwls20@gmail.com>
2024-01-17 22:27:02 +0900

    NPE 에러 수정

--- healingmeal/src/main/java/gsc/healingmeal/data/repository/MainDishCategoryRepository.java
+++ healingmeal/src/main/java/gsc/healingmeal/data/repository/MainDishCategoryRepository.java
@@ -4,7 +4,9 @@ import gsc.healingmeal.data.domain.MainDishCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MainDishCategoryRepository extends JpaRepository<MainDishCategory, Long> {
-    MainDishCategory findById(long user_id);
+    Optional<MainDishCategory> findById(long user_id);
 }
